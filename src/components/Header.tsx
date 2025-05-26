101de2ba58edf557b88f00798a4205d4cc5410d6
boba-milktea <catherine.idv@gmail.com>
2025-05-26 11:37:20 +0200

    second fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import popCorn from '../assets/pop-corn.png';
+
 const Header = () => {
   return (
     <header className="flex flex-col items-center py-8 text-center ">
       <img
         className="size-40 hover:rotate-30 duration-400 ease-in-out"
-        src="/src/assets/pop-corn.png"
+        src={popCorn}
         alt="icon"
       />
       <h1 className="font-roboto font-bold uppercase text-4xl text-graphite-gray">
